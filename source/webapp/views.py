fabcafaa3a3000473ade66a73315ea04188adecd
Shirin_Musaeva <shirinmusaeva26@gmail.com>
2023-05-24 10:01:18 +0600

    validation in create and update

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render, get_object_or_404, redirect
 from webapp.models import Task, STATUS_CHOICES
 
@@ -17,13 +18,30 @@ def task_create_views(request):
     if request.method == 'GET':
         return render(request, 'task_create.html', {'statuses': STATUS_CHOICES})
     elif request.method == 'POST':
+        errors = {}
         title = request.POST.get('title')
         description = request.POST.get('description')
         status = request.POST.get('status')
         to_do_date = request.POST.get('to_do_date')
+
         if not to_do_date:
             to_do_date = None
+
         new_task = Task.objects.create(title=title, description=description, status=status, to_do_date=to_do_date)
+
+        if not title:
+            errors['title'] = 'Title should be not empty'
+        elif len(title) > 50:
+            errors['title'] = 'Title should be 50 symbols or less'
+        if not description:
+            errors['description'] = 'Description should be not empty'
+        elif len(title) > 3000:
+            errors['description'] = 'Description should be 3000 symbols or less'
+
+        if errors:
+            return render(request, 'task_create.html', {'statuses': STATUS_CHOICES, 'errors': errors, 'task': new_task})
+
+        new_task.save()
         return redirect('task_view', pk=new_task.pk)
 
 def task_update_view(request, pk):
@@ -31,10 +49,26 @@ def task_update_view(request, pk):
     if request.method == 'GET':
         return render(request, 'task_update.html', {'task': task, 'statuses': STATUS_CHOICES})
     elif request.method == 'POST':
+        errors = {}
         task.status = request.POST.get('status')
         task.title = request.POST.get('title')
         task.description = request.POST.get('description')
         task.to_do_date = request.POST.get('to_do_date')
+
+        if not task.title:
+            errors['title'] = 'Title should be not empty'
+        elif len(task.title) > 50:
+            errors['title'] = 'title should be 50 symbols or less'
+        if not task.description:
+            errors['description'] = 'description should be not empty'
+        elif len(task.title) > 3000:
+            errors['description'] = 'description should be 3000 symbols or less'
+        if not task.to_do_date:
+            task.to_do_date = None
+
+        if errors:
+            return render(request, 'task_update.html', {'statuses': STATUS_CHOICES, 'errors': errors})
+
         task.save()
         return redirect('task_view', pk=task.pk)
 
